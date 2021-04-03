b461a110ea00bc4b28557663dcd7997914a0b0fc
sanusiwasiu <was.sanusi@gmail.com>
2021-04-03 20:10:52 +0100

    week5 update

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -1,4 +1,4 @@
-class DataModel {
+/*class DataModel {
     constructor() {
         this.data = [];
     }
@@ -8,7 +8,7 @@ class DataModel {
     }
 
     getById(id) {
-
+        return this.data.find(objs => objs.id) || null
     }
 
     save(obj) {
@@ -20,11 +20,71 @@ class DataModel {
     }
 
     update(obj, id) {
-
+        this.data[id] = obj
+        if (this.data.find(objs => objs.id) !== undefined){
+            return true
+        }else{
+            return false
+        }
     }
 
     delete(id) {
+        this.data.splice(indexOf(this.data[id]), 1)
+        if (this.data.find(objs => objs.id) !== undefined){
+            return true
+        }else{
+            return false
+        }
+    }
 
+    // this method will be overriden in the sub classes
+    validate(obj) {
+        return false;
+    }
+}
+*/
+// Do not worry about the below for now; It is included so that we can test your code
+// We will cover module exports in later parts of this course
+
+class DataModel {
+    constructor() {
+        this.data = [];
+    }
+
+    getAll() {
+        return this.data;
+    }
+
+    getById(id) {
+        return this.data.find(objs => objs.id === id) || null
+    }
+
+    save(obj) {
+        if (this.validate(obj)) {
+            this.data.push(obj);
+            return true;
+        }
+        return false;
+    }
+
+    update(obj, id) {
+        obj = {'id': id}
+        this.data.push(obj)
+        if (this.data.length > 0){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    delete(id) {
+        let omo = this.data.find(objs => objs.id == id)
+        this.data.splice(this.data.indexOf(omo), 1)
+        if (this.data.includes(omo)){
+            return false
+        }else{
+            return true
+        }
     }
 
     // this method will be overriden in the sub classes
