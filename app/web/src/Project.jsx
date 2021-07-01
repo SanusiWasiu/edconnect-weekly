5be92ee8dee53f346ef6de06bd41b556987ceaf7
sanusiwasiu <was.sanusi@gmail.com>
2021-07-01 07:19:22 +0100

    more changes

---
+++ app/web/src/Project.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, FormControl, FormGroup, FormLabel, Button, Form} from 'react-bootstrap'
+import { useHistory } from 'react-router'
+import Layout from './shared/Layout'
+
+const Project = () => {
+    return (
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+export default Project
