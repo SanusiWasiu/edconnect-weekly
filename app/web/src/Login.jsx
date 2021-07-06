927da12f650a8ccbe2b33b4e09f477349d0c4642
sanusiwasiu <was.sanusi@gmail.com>
2021-07-06 07:47:45 +0100

    Completed Week 7 Exercise

--- app/web/src/Login.jsx
+++ app/web/src/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     }
 
     const HandleSubmit = event =>{
-        event.preventDefaualt();
+        event.preventDefault();
         
         useEffect(()=> {
             let postData = {
