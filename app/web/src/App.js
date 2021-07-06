abf4c4f4c99b64c22b4479086abc0fcaa9bfefea
sanusiwasiu <was.sanusi@gmail.com>
2021-07-06 23:25:09 +0100

    Completed Week 7 Exercise

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -12,13 +12,12 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact={true} path='/' component={Home} />
-        <Route exact={true} path='/Home' component={Home} />
+      <Route path='/' exact={true} component={Home} />
         <Route path='/signup' component={Signup} />
-        <Route path= '/login' component={Login} />
-        <Route exact={true} path='/projects/submit' component={CreateProject} />
-        <Route exact={true} path='/projects/:id' component={Project} />
-        <Route path="*" render={()=><Layout><div className="text-center p-5"><h1>Oops!!! Page not found</h1></div></Layout> }/>
+        <Route path='/login' component={Login} />
+        <Route path='/projects/submit' component={CreateProject} />
+        <Route path='/projects' component={Project} />
+        <Route path='/projects/:id' component={Project} />
       </Switch>
     </Router>
   );
